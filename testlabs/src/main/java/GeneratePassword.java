4bdee4ba154cd35b360b4a7736d47b4fb4c746c3
Ajeet Gupta <agupta2@illumina.com>
2015-09-08 22:37:29 -0700

    New fiels committed

---
+++ testlabs/src/main/java/GeneratePassword.java
@@ -0,0 +1,8 @@
+/**
+ * Created by agupta2 on 9/3/15.
+ */
+public class GeneratePassword {
+    public static void main(String[] args) {
+        System.out.println(args[0].toUpperCase());
+    }
+}
